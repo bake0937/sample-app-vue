90e0fad896608c7d5c88afd6b124e2a36987a4cb
bake0937 <sadayasu.info@gmail.com>
2019-06-29 17:42:20 +0900

    インデント修正

--- src/views/AddressForm.vue
+++ src/views/AddressForm.vue
@@ -9,12 +9,12 @@
         <v-card>
           <v-card-text>
             <v-form>
-               <v-text-field v-model="address.name" label="名前"></v-text-field>
-               <v-text-field v-model="address.tel" label="電話番号"></v-text-field>
-               <v-text-field v-model="address.email" label="メールアドレス"></v-text-field>
-               <v-text-field v-model="address.address" label="住所"></v-text-field>
-               <v-btn @click="$router.push({ name: 'addresses' })">キャンセル</v-btn>
-               <v-btn color="info" @click="submit">保存</v-btn>
+              <v-text-field v-model="address.name" label="名前"></v-text-field>
+              <v-text-field v-model="address.tel" label="電話番号"></v-text-field>
+              <v-text-field v-model="address.email" label="メールアドレス"></v-text-field>
+              <v-text-field v-model="address.address" label="住所"></v-text-field>
+              <v-btn @click="$router.push({ name: 'addresses' })">キャンセル</v-btn>
+              <v-btn color="info" @click="submit">保存</v-btn>
             </v-form>
           </v-card-text>
         </v-card>
